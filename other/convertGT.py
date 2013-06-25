5336bb480cfa4c578bf9028a3b66d9737f473779
vikramb <balachristmas@gmail.com>
2013-06-24 17:52:14 -0700

    conversion files working

--- other/convertGT.py
+++ other/convertGT.py
@@ -1,4 +1,3 @@
-from gnuradio import gr  
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
@@ -33,10 +32,13 @@ def main():
 
     date = ''   #'2013_04_21'
     file_date = 'home3747_E_'+date+'.txt'
-    path = '/home/henry/Vikram_stuff/GroundTruth/EX2-GroundTruth/'
-    path_save = '/home/henry/Vikram_stuff/GroundTruth/'
+    #path = '/home/henry/Vikram_stuff/GroundTruth/EX2-GroundTruth/'
+    path = '/Users/Vikram/Dropbox/PowCapMobile_Experiments/EX2-GroundTruth/'
+    #path_save = '/home/henry/Vikram_stuff/GroundTruth/'
+    path_save = '/Users/Vikram/Dropbox/PowCapMobile_Experiments/formatted_GT/'
     path_temp = '/home/henry/Vikram_stuff/tools/other/'
     panel_title = "ted5000_mainpanel" 
+    #panel_title = "ted5000_subpanel"
 
 
     for r,d,f in os.walk(path):
@@ -67,12 +69,29 @@ def main():
                         piece.replace("[", "")
                         piece.replace("]", "")
 
-                    temp_list = [] 
-                    temp_list.append(temp_row[0])
-                    temp_list.append(temp_row[2])
-                    temp_list.append(temp_row[4])
-                    temp_list.append(temp_row[6])
-                    temp_list.append(temp_row[8]) 
+                    if panel_title == "ted5000_mainpanel":
+                        temp_list = [] 
+                        temp_list.append(temp_row[0])
+                        temp_list.append(temp_row[2])
+                        temp_list.append(temp_row[4])
+                        temp_list.append(temp_row[6])
+                        temp_list.append(temp_row[8]) 
+                    elif panel_title == "ted5000_subpanel":
+                        temp_list = [] 
+                        temp_list.append(temp_row[0])
+                        temp_list.append(temp_row[2])
+                        temp_list.append(temp_row[4])
+                        temp_list.append(temp_row[5])
+                        temp_list.append(temp_row[6]) 
+
+                        temp_list.append(temp_row[8])
+                        temp_list.append(temp_row[9])
+                        temp_list.append(temp_row[10])
+
+                        temp_list.append(temp_row[11])
+                        temp_list.append(temp_row[12]) 
+                        temp_list.append(temp_row[13]) 
+
 
                     dat_main.append([element.strip('[] ') for element in temp_list])
                     #dat_main.append(temp_list)
@@ -80,7 +99,11 @@ def main():
             #print len(dat_main)
             #print dat_main
 
-        title_list = ['time', 'panel', 'voltage', 'real power', 'apparent power']
+        if panel_title == "ted5000_mainpanel":
+            title_list = ['time', 'panel', 'voltage', 'real power', 'apparent power']
+        elif panel_title == "ted5000_subpanel":
+            title_list = ['time', 'panel', 'voltage1', 'voltage2', 'voltage3', 'real power1', 'real power2', 'real power3', 'apparent power1', 'apparent power2', 'apparent power3']
+
         dat_main[0] = title_list
 
         with open(path_save+date+'_'+panel_title+'.csv', 'w') as writeobj:
@@ -93,77 +116,3 @@ def main():
 
 
 main()
-
-'''
-class FindFile():
-    def __init__(self, folderPath, date, time):
-
-        
-        self.path = folderPath
-        self.date = date
-        self.time = time
-        self.datetime = datetime.strptime( self.date + self.time , '%y%m%d%H%M%S' )
-
-        log.info("Searching for file containing data at...")
-        log.info(self.datetime)
-        log.info("...in path: "+self.path)
-
-        for r,d,f in os.walk(self.path):
-
-            #print f
-
-            file_list = f
-            
-            #we only want to look at given path and not any deeper dirs
-            break
-
-        if len(file_list) == 0:
-            log.warning("No files found in given path!")
-        else:
-            log.info("%d files found in path" % len(file_list))
-            #check for files with word "mains" in them
-            file_list = [k for k in file_list if 'mains' in k]
-
-            #check for files with required start date
-            #file_list = [k for k in file_list if self.date in k]
-
-            found_list = []
-
-            file_list.sort()
-
-            #latest files for quick look
-            self.latestFiles = [ file_list[-4] , file_list[-3], file_list[-2] , file_list[-1] ]
-
-            for files in file_list:
-                datetime_temp = datetime.strptime( files[6:12] + files[13:19] , '%y%m%d%H%M%S' )
-                if self.datetime >= datetime_temp:
-                    found_list.append(files)
-                    #found_list.append( [k for k in f if files[6:18] in k] )
-                    #file_list.sort() 
-
-            #print found_list
-            #file_list = [k for k in f if found_list in k]
-            file_list = found_list
-            file_list.sort()
-            #print file_list
-            #print len(found_list)
-
-            if len(file_list) == 0:
-                log.warning("No file found with requested date")
-            else:
-                self.foundPath = [ file_list[-2] , file_list[-1] ]
-                log.info("Found data files: %s" % ' , ' .join(map(str,self.foundPath)))
-                self.checkFile()
-'''
-
-'''
-if __name__ == '__main__':
-    try:
-        fileName = '/home/henry/NeslStore/vikram/powcapData/Jason-Drive/mains_130313_030126-chan_1.dat'
-        start_at_second = 10
-        end_at_second = 15
-        window_second = 200e-3
-        p = ParseData(fileName, start_at_second, end_at_second, window_second)
-    except KeyboardInterrupt:
-        log.info("Exiting")
-'''
--- other/rawTtab.py
+++ other/rawTtab.py
@@ -95,11 +95,12 @@ def main():
     #endpoint = (endSec-startSec) * samp_rate       #set endpoint in file
     #fileName = foundPath                #set file path
 
-    print f
     #print path+str(f)
     fileName = [ path+str(f[0]), path+str(f[0]) ]
 
-    print fileName
+    log.info("")
+    log.info("Files found: %s" % f)
+    log.info("")
     
     win_size = int( winSec * samp_rate )#set window size in file
     #note, self.samp_rate = 1 second of samples for each channel
@@ -108,65 +109,101 @@ def main():
     fileobj2 = open(fileName[1])
     jcount = 0
     count = 0
-    chunk_size = win_size*16 #reads 200K steps into file = 200ms
+    chunk_size = win_size*16 #reads winSec * samp_rate * 16 bits into file.
     header_array = ["V1", "C2", "V3", "C4"] 
-    dat1 = []
-    dat2 = []
-    dat3 = []
-    dat4 = []
     #out_array = [ [] ]
 
-    while count < 60:
-        chunk = fileobj.read( chunk_size ) 
-        chunk2 = fileobj2.read( chunk_size )
-        Data = np.reshape( np.frombuffer(chunk, dtype=scipy.complex64), (-1, 1))#self.num_channels))
-        Data2 = np.reshape( np.frombuffer(chunk2, dtype=scipy.complex64), (-1, 1))#self.num_channels))
-        #print Data
-        print len(Data)
-        print len(Data2)
+    size1 = os.fstat(fileobj.fileno()).st_size 
+    size2 = os.fstat(fileobj2.fileno()).st_size 
+    #log.info("Size of file1 is %s" % str(size1))
+    #log.info("Size of file2 is %s" % str(size2))
+    #log.info("Number of windows in file is: %s" % str(size1/chunk_size))
+    if size1 == size2:
+        wins_in_file = size1/chunk_size
+    else:
+        log.info("Warning, file size mismatch.")
+        wins_in_file = size1/chunk_size
 
-        #count = count+1 
-        count = 60
-        
-        #down1 = downsample_to_proportion(Data, .001)
-        #down2 = downsample_to_proportion(Data2, .001)
-        #print len(down1)
-        #print len(down2)
-
-        dat1 = downsample_to_proportion(Data.real, .001)
-        dat2 = downsample_to_proportion(Data.imag, .001)
-        dat3 = downsample_to_proportion(Data2.real, .001)
-        dat4 = downsample_to_proportion(Data2.imag, .001)
-        print len(dat1)
-        print len(dat2)
-        print len(dat3)
-        print len(dat4)
-
-        '''
-        #out_array = [down1.real, down1.imag, down2.real, down2.imag] 
-        #print len(out_array)
-        array1.append(dat1)
-        array2.append(dat2)
-        array3.append(dat3)
-        array4.append(dat4)
-        print len(array1)
-        '''
-
-
-    dat1.insert(0, 'V1')
-    dat2.insert(0, 'C2')
-    dat3.insert(0, 'V3')
-    dat4.insert(0, 'C4')
-    out_array = [dat1, dat2, dat3, dat4]
-    print len(out_array)
-    print len(out_array[0])
-
-    
+    #open file to begin writing CSV document
     pathSave = '/Users/Vikram/Dropbox/PowCapMobile_scripts/tools/other/' 
     with open(pathSave+'mains'+'.csv', 'w') as writeobj:
         log.info("Writing to: "+'mains.csv')
         write_gT = csv.writer(writeobj, 'excel-tab')
-        write_gT.writerows(out_array)
+
+        #ctlimit = 2
+        ctlimit = wins_in_file
+        for count in range(0, ctlimit):
+
+            if winSec == 1:
+                log.info("Using a window size of %d second:" % winSec)
+            else:
+                log.info("Using a window size of %d seconds:" % winSec)
+
+            log.info("Converting window %d of %d." % (count+1, ctlimit) )
+
+            chunk = fileobj.read( chunk_size ) 
+            chunk2 = fileobj2.read( chunk_size )
+            Data = np.reshape( np.frombuffer(chunk, dtype=scipy.complex64), (-1, 1))#self.num_channels))
+            Data2 = np.reshape( np.frombuffer(chunk2, dtype=scipy.complex64), (-1, 1))#self.num_channels))
+            #print len(Data)
+            #print len(Data2)
+
+            #count = count+1 
+            #count = 60
+            
+            #down1 = downsample_to_proportion(Data, .001)
+            #down2 = downsample_to_proportion(Data2, .001)
+            #print len(down1)
+            #print len(down2)
+
+            dat1 = []
+            dat2 = []
+            dat3 = []
+            dat4 = []
+
+            dat1 = downsample_to_proportion(Data.real, .001)
+            dat2 = downsample_to_proportion(Data.imag, .001)
+            dat3 = downsample_to_proportion(Data2.real, .001)
+            dat4 = downsample_to_proportion(Data2.imag, .001)
+
+            #print len(dat1)
+            #print len(dat2)
+            #print len(dat3)
+            #print len(dat4)
+
+            if count<1:
+                dat1.insert(0, 'V1')
+                dat2.insert(0, 'C2')
+                dat3.insert(0, 'V3')
+                dat4.insert(0, 'C4')
+
+            #out_array = [dat1, dat2, dat3, dat4]
+            out_array = [ [] ]
+            
+            for i in range(0, len(dat1)):
+                temp_array = []
+
+                temp_array.append(dat1[i])
+                temp_array.append(dat2[i])
+                temp_array.append(dat3[i])
+                temp_array.append(dat4[i])
+                    
+                #temp_array = [dat1[i], dat2[i], dat3[i], dat4[i]]
+                
+
+                #out_array.append(temp_array)
+                out_array.append([str(element).strip('[] ') for element in temp_array])
+
+                #print len(out_array)
+
+            #print len(out_array)
+            #print len(out_array[0])
+
+            #remove infuriating blank first index in out_array
+            discard = out_array.pop(0)
+            #log.info(discard)
+
+            write_gT.writerows(out_array)
     
     raw_input("press any key to exit")
 
